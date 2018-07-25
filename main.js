4bd4530604bac9a9f06d80c75335b13fec2dc62e
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-25 20:06:00 +0700

    restructured web page

--- main.js
+++ main.js
@@ -7,10 +7,46 @@ submitLogin.onclick = function() {
     alert("Key Confirmed!");
     return false;
   } else if (keyValue === "admin1234") {
-    alert("admin!")
+    let homerCount = sessionStorage.homerCount;
+    let spongebobCount = sessionStorage.spongebobCount;
+    alert(`Homer: ${homerCount}\nSpongeBob: ${spongebobCount}`);
+    sessionStorage.homerCount = 0;
+    sessionStorage.spongebobCount = 0;
     return false;
   } else {
     alert("Invalid Key!");
     return false;
   }
 };
+
+var voteClickHomer = document.getElementsByClassName('votebtn')[0];
+
+voteClickHomer.onclick = function() {
+  // if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.homerCount) {
+      sessionStorage.homerCount = Number(sessionStorage.homerCount) + 1;
+    } else {
+      sessionStorage.homerCount = 1;
+    }
+  // }
+  console.log("homer -> " + sessionStorage.homerCount);
+  console.log("spongebob -> " + sessionStorage.spongebobCount);
+  alert("Thank you for voting");
+  window.location = "index.html";
+}
+
+var voteClickSpongebob = document.getElementsByClassName('votebtn')[1];
+
+voteClickSpongebob.onclick = function() {
+  // if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.spongebobCount) {
+      sessionStorage.spongebobCount = Number(sessionStorage.spongebobCount) + 1;
+    } else {
+      sessionStorage.spongebobCount = 1;
+    }
+  // }
+  console.log("homer -> " + sessionStorage.homerCount);
+  console.log("spongebob -> " + sessionStorage.spongebobCount);
+  alert("Thank you for voting");
+  window.location = "index.html";
+}
